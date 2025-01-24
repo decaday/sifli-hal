85a118346f316e4eba93966643eb6e9bc8b7619a
decaday <myDecaday@outlook.com>
2025-01-24 14:55:14 +0800

    setup project structure, update readme

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-# sifli-hal
+# SiFli HAL
 
-Hardware Abstraction Layer (HAL) for SiFli MCUs
+[![Crates.io][badge-license]][crates]
+[![Crates.io][badge-version]][crates]
+[![docs.rs][badge-docsrs]][docsrs]
+
+[badge-license]: https://img.shields.io/crates/l/sifli-hal?style=for-the-badge
+[badge-version]: https://img.shields.io/crates/v/sifli-hal?style=for-the-badge
+[badge-docsrs]: https://img.shields.io/docsrs/sifli-hal?style=for-the-badge
+[crates]: https://crates.io/crates/sifli-hal
+[docsrs]: https://docs.rs/sifli-hal
+
+Rust Hardware Abstraction Layer (HAL) for SiFli MCUs.
+
+> [!WARNING]
+> 
+> This project is still in its early stages and is **not** yet usable.
+
+## License
+
+This project is licensed under either of
+
+- Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or
+  <http://www.apache.org/licenses/LICENSE-2.0>)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or <http://opensource.org/licenses/MIT>)
+
+at your option.
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,122 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![no_std]
+#![doc = include_str!("../README.md")]
+
+// This mod MUST go first, so that the others see its macros.
+pub(crate) mod fmt;
+
+mod chip;
+use chip::*;
+
+#[cfg(feature = "unstable-pac")]
+pub use sifli_pac as pac;
+#[cfg(not(feature = "unstable-pac"))]
+pub(crate) use sifli_pac as pac;
+
+/// HAL configuration for RP.
+pub mod config {
+    // use crate::clocks::ClockConfig;
+
+    /// HAL configuration passed when initializing.
+    #[non_exhaustive]
+    pub struct Config {
+        // /// Clock configuration.
+        // pub clocks: ClockConfig,
+    }
+
+    impl Default for Config {
+        fn default() -> Self {
+            Self {
+                // clocks: ClockConfig::crystal(12_000_000),
+            }
+        }
+    }
+
+    impl Config {
+        // /// Create a new configuration with the provided clock config.
+        // pub fn new(clocks: ClockConfig) -> Self {
+        //     Self { clocks }
+        // }
+
+        pub fn new() -> Self {
+            Self {}
+        }
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Initialize the `sifli-hal` with the provided configuration.
+///
+/// This returns the peripheral singletons that can be used for creating drivers.
+///
+/// This should only be called once at startup, otherwise it panics.
+pub fn init(_config: config::Config) -> Peripherals {
+    // Do this first, so that it panics if user is calling `init` a second time
+    // before doing anything important.
+    let peripherals = Peripherals::take();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    // unsafe {
+    //     clocks::init(config.clocks);
+    //     #[cfg(feature = "time-driver")]
+    //     time_driver::init();
+    //     dma::init();
+    //     gpio::init();
+    // }
+
+    peripherals
+}
+
+/// Macro to bind interrupts to handlers.
+///
+/// This defines the right interrupt handlers, and creates a unit struct (like `struct Irqs;`)
+/// and implements the right [`Binding`]s for it. You can pass this struct to drivers to
+/// prove at compile-time that the right interrupts have been bound.
+///
+/// Example of how to bind one interrupt:
+///
+/// ```rust,ignore
+/// use sifli_hal::{bind_interrupts, usb, peripherals};
+///
+/// bind_interrupts!(struct Irqs {
+///     USBCTRL_IRQ => usb::InterruptHandler<peripherals::USB>;
+/// });
+/// ```
+///
+// developer note: this macro can't be in `embassy-hal-internal` due to the use of `$crate`.
+#[macro_export]
+macro_rules! bind_interrupts {
+    ($vis:vis struct $name:ident {
+        $(
+            $(#[cfg($cond_irq:meta)])?
+            $irq:ident => $(
+                $(#[cfg($cond_handler:meta)])?
+                $handler:ty
+            ),*;
+        )*
+    }) => {
+        #[derive(Copy, Clone)]
+        $vis struct $name;
+
+        $(
+            #[allow(non_snake_case)]
+            #[no_mangle]
+            $(#[cfg($cond_irq)])?
+            unsafe extern "C" fn $irq() {
+                $(
+                    $(#[cfg($cond_handler)])?
+                    <$handler as $crate::interrupt::typelevel::Handler<$crate::interrupt::typelevel::$irq>>::on_interrupt();
+
+                )*
+            }
+
+            $(#[cfg($cond_irq)])?
+            $crate::bind_interrupts!(@inner
+                $(
+                    $(#[cfg($cond_handler)])?
+                    unsafe impl $crate::interrupt::typelevel::Binding<$crate::interrupt::typelevel::$irq, $handler> for $name {}
+                )*
+            );
+        )*
+    };
+    (@inner $($t:tt)*) => {
+        $($t)*
     }
 }
